7b68fa1b4e05fea1794f023ce540730fddfc2961
evaferrerasbr <evaferrerasbr@gmail.com>
2020-12-21 19:19:33 +0100

    Show initial time in clocks

--- src/components/WhitesRight.js
+++ src/components/WhitesRight.js
@@ -1,28 +1,28 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Clock from './Clock';
 import '../stylesheets/WhitesRight.scss';
 
 function WhitesRight(props) {
-  const { totalMinutes, incAfterEachPlay, playNumber, incOfMinutes } = props;
+  const { totalMinutes } = props;
+
+  const [whiteCounter, setWhiteCounter] = useState(totalMinutes * 60);
+  const [blackCounter, setBlackCounter] = useState(totalMinutes * 60);
 
   return (
     <section className="WhitesRight">
       <h2 className="WhitesRight__title">
         Step 3: Start and press any key to change turns
       </h2>
+      <button className="WhitesRight__button" type="button">
+        Start
+      </button>
+      <button className="WhitesRight__button" type="button">
+        Stop
+      </button>
       <article className="WhitesRight__clocks">
-        <Clock
-          totalMinutes={parseInt(totalMinutes)}
-          incAfterEachPlay={parseInt(incAfterEachPlay)}
-          playNumber={parseInt(playNumber)}
-          incOfMinutes={parseInt(incOfMinutes)}
-        />
-        <Clock
-          totalMinutes={parseInt(totalMinutes)}
-          incAfterEachPlay={parseInt(incAfterEachPlay)}
-          playNumber={parseInt(playNumber)}
-          incOfMinutes={parseInt(incOfMinutes)}
-        />
+        <Clock counter={whiteCounter} />
+        <Clock counter={blackCounter} />
       </article>
       <button className="WhitesRight__button" type="button">
         <Link className="WhitesRight__button--link" to="/colours">
