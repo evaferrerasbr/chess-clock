50578ae7413c304e18884fd7cadbf9b0ed98bec3
evaferrerasbr <evaferrerasbr@gmail.com>
2020-12-21 17:33:11 +0100

    Add commit to fix github problem

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,9 +1,11 @@
 import '../stylesheets/Clock.scss';
 
-function Clock() {
+function Clock(props) {
+  const { totalMinutes, incAfterEachPlay, playNumber, incOfMinutes } = props;
+
   return (
     <article className="Clock">
-      <p></p>
+      <p>{totalMinutes}</p>
     </article>
   );
 }
